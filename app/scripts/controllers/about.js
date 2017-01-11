5af2ba0f35ad1ef72dff908322fbb02e52502651
vidwath <vpampapathi@qwinix.io>
2017-01-11 12:29:50 +0530

    angular app setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quizappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the quizappApp
+ */
+angular.module('quizappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quizappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quizappApp
+ */
+angular.module('quizappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
